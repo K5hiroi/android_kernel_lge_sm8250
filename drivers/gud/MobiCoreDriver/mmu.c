a657df83a15c519b171472838a1abb0169ad68e5
pnguyen879 <howardson2001@gmail.com>
2026-06-02 17:38:17 -0500

    MobiCoreDriver: Move from mmap_sem to mmap_lock
    Change-Id: I50ac8e86258e7385fc59746fd53b8c18f5997f99

--- drivers/gud/MobiCoreDriver/mmu.c
+++ drivers/gud/MobiCoreDriver/mmu.c
@@ -475,7 +475,7 @@ struct tee_mmu *tee_mmu_create(struct mm_struct *mm,
 			long gup_ret;
 
 			/* Buffer was allocated in user space */
-			down_read(&mm->mmap_sem);
+			down_read(&mm->mmap_lock);
 			/*
 			 * Always try to map read/write from a Linux PoV, so
 			 * Linux creates (page faults) the underlying pages if
@@ -504,7 +504,7 @@ struct tee_mmu *tee_mmu_create(struct mm_struct *mm,
 							   nr_pages, 0, pages);
 			}
 #endif
-			up_read(&mm->mmap_sem);
+			up_read(&mm->mmap_lock);
 			if (gup_ret < 0) {
 				ret = gup_ret;
 				mc_dev_err(ret, "failed to get user pages @%p",
